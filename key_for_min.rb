f3514b8edbc2657a11897fda1649959bf84ef713
Kenny Hom <kenny.hom27@gmail.com>
2017-11-20 16:41:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,10 @@
 def key_for_min_value(name_hash)
   min_value = 100
   min_name = nil
-  name_hash.each { |name, value| min_name = name, min_value = value if value < min_value}
+  name_hash.each do |name, value|
+    if value < min_value
+      min_name = name, min_value = value
+    end
+  end
   min_name
 end
